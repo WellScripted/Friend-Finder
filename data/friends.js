99f64553faaaced1d87aae86b5c8a1236aa94eb3
Scripted Well <whigareda@theblackpilledpage.com>
2019-01-31 21:12:20 -0700

    Rough Draft (Styling, Buttons, Functions, etc.)

--- data/friends.js
+++ data/friends.js
@@ -1,14 +1,34 @@
 var friends = [
     {
-        name: "Joe",
-        photo: "#",
+        name: "Joe Blake",
+        photo: "https://www.black-leatherjacket.com/image/cache/data/Joe-blake-the-man-in-the-high-castle-jacket/he-man-in-the-high-castle-jacket-900x900.jpg",
         scores: ["1", "1", "2", "5", "4", "3", "2", "2", "5"]
     },
     {
-        name: "Bob",
-        photo: "#",
+        name: "Juliana Crain",
+        photo: "https://vignette.wikia.nocookie.net/the-man-in-the-high-castle/images/8/89/Juliana_C2.jpg/revision/latest?cb=20161013221122",
         scores: ["5", "1", "2", "5", "2", "3", "2", "2", "5"]
-    } //ADD 4 MORE
-    
+    },
+    {
+        name: "JP Sears",
+        photo: "http://www.spiritualmediablog.com/wp-content/uploads/2016/10/14311234_1422262404456284_5823811022914283866_o.jpg",
+        scores: ["5", "1", "2", "5", "2", "3", "2", "2", "5"]
+    },
+    {
+        name: "Courtney Love",
+        photo: "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/5/8/1399551131914/Courtney-Love-011.jpg?width=300&quality=85&auto=format&fit=max&s=53a9d0eb03a005e54c3972213376dd80",
+        scores: ["5", "1", "2", "5", "2", "3", "2", "2", "5"]
+    },
+    {
+        name: "Commander Fred Waterford",
+        photo: "https://static.pressfrom.info/upload/images/real/2018/07/27/the-handmaids-tale-the-scene-joseph-fiennes-refused-to-do__428117_.jpg",
+        scores: ["5", "1", "2", "5", "2", "3", "2", "2", "5"]
+    },
+    {
+        name: "June 'Offred' Osborne",
+        photo: "https://cdn.vox-cdn.com/thumbor/OTw9p7UbZWKiOEKi6H08ZxMB2RA=/0x0:766x436/1200x800/filters:focal(243x143:365x265)/cdn.vox-cdn.com/uploads/chorus_image/image/54437833/handmaid_elisabeth.0.jpg",
+        scores: ["1", "5", "5", "2", "3", "5", "5", "5", "2", "1"]
+    },
+
 ]
 module.exports = friends;
